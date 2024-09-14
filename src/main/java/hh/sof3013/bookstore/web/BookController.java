5157d1712bb7963fa7091f515e37d0e6358c2965
kvilho <bhi06@myy.haaga-helia.fi>
2024-09-15 01:19:34 +0300

    Osa 3

--- src/main/java/hh/sof3013/bookstore/web/BookController.java
+++ src/main/java/hh/sof3013/bookstore/web/BookController.java
@@ -13,6 +13,11 @@ import hh.sof3013.bookstore.domain.Book;
 import hh.sof3013.bookstore.domain.BookRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -28,6 +33,27 @@ public class BookController {
         model.addAttribute("books", books);
         return "booklist";
     }
+
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+    
+    @PostMapping("/savebook")
+    public String saveBook(@ModelAttribute Book book) {
+        repository.save(book);
+        
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/deletebook/{id}")
+    public String deleteBook(@PathVariable Long id, Model model) {
+        repository.deleteById(id);
+        return "redirect:/booklist";
+    }
+    
+    
 
     
 }
